610e551668b3d28fddaf1d42e18442ff0fe4820a
Garvit Man Singh <garvitmansingh830@gmail.com>
2025-06-18 21:07:42 +0530

    Fix: Added CORS middleware to handle preflight requests

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from fastapi.responses import JSONResponse
 from rag_pipeline import generate_answer
 from typing import Optional
 from pydantic import BaseModel,Field
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify domain(s) if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class QuestionRequest(BaseModel):
     question: str = Field(..., description="Ask questions related to the TDS course.")
     image: Optional[str] = Field(default=None, description="Base64 encoded image string")
